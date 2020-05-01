4ee7b142d8c86a75a867b895026da17ad32ff42a
blank <48980484+adebarkah@users.noreply.github.com>
2020-05-01 15:37:03 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt install figlet -y
+apt install toilet -y
+apt install ruby -y
+apt install python
+apt install python2
+apt install nano
+apt install toilet
+gem install lolcat
+pip install requests
+apt update
+apt upgrade
+
